b55f5e6bf8b1b034b66657f87c68abdb0d5ba85a
Peter Volf <do.volfp@gmail.com>
2026-02-25 10:06:43 +0100

    add gzip middleware

--- basecoat_app/main.py
+++ basecoat_app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi.staticfiles import StaticFiles
 from holm import App
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 App(app=app)
