67d9fff3e97a43d87e64d752cac0d27c81d48d82
Peter Volf <do.volfp@gmail.com>
2026-07-28 10:52:47 +0200

    add ButtonSize type

--- htmui/basecoat/typing.py
+++ htmui/basecoat/typing.py
@@ -5,3 +5,5 @@ Align: TypeAlias = Literal["start", "center", "end"]
 Side: TypeAlias = Literal["top", "bottom", "left", "right", "inline-start", "inline-end"]
 
 ButtonVariant: TypeAlias = Literal["outline", "secondary", "ghost", "destructive", "link"]
+
+ButtonSize: TypeAlias = Literal["xs", "sm", "default", "lg", "icon", "icon-xs", "icon-sm", "icon-lg"]
